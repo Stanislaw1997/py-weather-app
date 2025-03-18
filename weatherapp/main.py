3a0759f0d02050a157da5c9aea0e48383ea3e168
Stanislaw <stanislawvr@gmail.com>
2025-03-18 04:43:26 -0300

    main.py
    Main Application Entry Point
    
    This script initializes the PyQt5 application, creates the 'WeatherApp' window, and starts the event loop to display the app.

---
+++ weatherapp/main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from weather_app import WeatherApp
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
