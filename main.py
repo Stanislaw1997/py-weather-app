1f2c32013c9b84dee6e1f3a0db616e038d7d0fe7
Stanislaw <stanislawvr@gmail.com>
2025-04-04 20:57:23 -0300

    Entry point of the Weather App
    Initializes the PyQt5 application and launches the main weather interface window

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from weather_app import WeatherApp
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    weather_app = WeatherApp()
-    weather_app.show()
-    sys.exit(app.exec_())
